df0773484b42f292cc3a4841aa7db48384d8e813
Wu <thanawat.w@tcc-technology.com>
2025-03-21 16:33:16 +0700

    fix bug yahtzee

---
+++ src/types/dice.ts
@@ -0,0 +1,2 @@
+export type Dice = 1 | 2 | 3 | 4 | 5 | 6;
+export type Dices = [Dice, Dice, Dice, Dice, Dice];
--- src/yahtzee/yahtzee.ts
+++ src/yahtzee/yahtzee.ts
@@ -1,9 +1,11 @@
-export function yahtzee(target: number, dice: number[]) {
+import { Dice, Dices } from "../types/dice";
+
+export function yahtzee(target: Dice, dice: Dices) {
   let sum = 0;
 
   if (dice.length !== 5) return "Dice array length must be 5 long";
 
-  if (target < 0 || target > 6) return "Target must between 1-6";
+  if (target < 1 || target > 6) return "Target must between 1-6";
 
   for (let i = 0; i < dice.length; i++) {
     if (dice[i] > 6) return "Number in dice must between 1-6";
